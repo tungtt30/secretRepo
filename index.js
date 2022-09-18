2155adf226bd9fa79219172675681082f2120b9b
tungtt30 <tungchipsctn2@gmail.com>
2022-09-18 23:58:17 +0700

    cors

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const PORT = process.env.PORT || 5000
 connectDB()
 
 // app.use('/api/song', mp3apiRouter)
+//cors
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
+
+
+/////
 app.get('/', mp3apiRouter)
 app.get('/api/music', mp3apiRouter)
 app.listen(PORT, () => {
